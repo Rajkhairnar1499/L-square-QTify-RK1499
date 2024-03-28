c7a7441a242196353fb4754201d475105487f182
Rajkhairnar1499 <rajkhairnar1499@gmail.com>
2024-03-29 02:29:10 +0530

    update navbar

--- qtify/src/App.js
+++ qtify/src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
+import React from "react"
 import './App.css';
+import { Route,Routes } from "react-router-dom"
+import LandingPage from "./mainpages/LandingPage";
+import AlbumsDetails from "./mainpages/AlbumsDetails/AlbumsDetails";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/album/:slug" element={<AlbumsDetails />} />
+        <Route path="*" element={<LandingPage />} />
+      </Routes>
+    
     </div>
   );
 }
